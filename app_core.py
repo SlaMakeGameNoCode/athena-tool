eaeed06e79469a12904397859ca9faa4e3ef38d2
Maicv <maicv@horusent.com>
2026-06-12 17:37:44 +0700

    fix(update): protect inner double quotes from cmd.exe /c stripping for path with space (v1.0.14)

--- app_core.py
+++ app_core.py
@@ -9,7 +9,7 @@ import os
 import sys
 import json
 
-APP_VERSION = "1.0.13"
+APP_VERSION = "1.0.14"
 
 app = FastAPI(title="Athena Assistant App")
 
@@ -1026,7 +1026,7 @@ def apply_update_endpoint():
                 if os.name == 'nt':
                     DETACHED_PROCESS = 0x00000008
                     CREATE_NEW_PROCESS_GROUP = 0x00000200
-                    cmd = f'ping 127.0.0.1 -n 3 > nul & start "" "{exe_path}" {args_str}'
+                    cmd = f'""ping 127.0.0.1 -n 3 > nul & start "" "{exe_path}" {args_str}""'
                     subprocess.Popen(cmd, shell=True, cwd=BASE_DIR, creationflags=DETACHED_PROCESS | CREATE_NEW_PROCESS_GROUP)
                 else:
                     os.execv(exe_path, [exe_path] + sys.argv)
