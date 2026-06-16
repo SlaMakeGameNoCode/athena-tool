2a904295b9b88910fbba9f5e3851969d64894eb5
Maicv <maicv@horusent.com>
2026-06-16 16:58:51 +0700

    release: update APP_VERSION in main.py to v1.0.32

--- main.py
+++ main.py
@@ -4,7 +4,7 @@ Không chứa logic app, chỉ load từ root folder."""
 import sys
 import os
 
-APP_VERSION = "1.0.30"
+APP_VERSION = "1.0.32"
 
 if getattr(sys, 'frozen', False):
     RUNNING_DIR = os.path.dirname(sys.executable)
